c9096f2384b950ee5506af327e1aeba53b449655
진영 <jin-yeong@Jinyoung-MacBook-Air.local>
2025-10-03 08:40:42 +0900

    feat: commit

--- infra/argo.tf
+++ infra/argo.tf
@@ -1,13 +1,11 @@
-# resource "helm_release" "argocd" {
-#   name       = "argocd"
-#   repository = "https://argoproj.github.io/argo-helm"
-#   chart      = "argo-cd"
-#   version    = "7.3.0" # 원하는 버전
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+  create_namespace = true
 
-#   namespace        = "argocd"
-#   create_namespace = true
-
-#   values = [
-#     file("argocd-values.yaml")
-#   ]
-# }
+  values = [
+    file("argocd-values.yaml")
+  ]
+}
--- infra/provider.tf
+++ infra/provider.tf
@@ -29,15 +29,8 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.this.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.this.token
-# }
-
-# provider "helm" {
-#   kubernetes = {
-#     config_path = "~/.kube/config"
-#   }
-# }
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
