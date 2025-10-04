9e0899ce2aaa3a9b571e59261582702661470fc7
진영 <jin-yeong@Jinyoung-MacBook-Air.local>
2025-10-04 10:33:42 +0900

    feat :

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -39,13 +39,14 @@ module "eks" {
       }
     }
   }
+  create_node_security_group =false
   eks_managed_node_groups = {
     default = {
       name = "default"
       instance_types = ["t3.medium"]
-      desired_size   = 1
-      min_size       = 1
-      max_size       = 1
+      desired_size   = 2
+      min_size       = 2
+      max_size       = 2
     }
   }
 
